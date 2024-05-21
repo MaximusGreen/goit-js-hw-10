e3605f1d02fcb9a82db62734d46c686967219f88
MaximusGreen <154276386+MaximusGreen@users.noreply.github.com>
2024-05-21 20:30:59 +0300

    1

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const delay = document.querySelector('input[name="delay"]').value;
+  const state = document.querySelector('input[name="state"]:checked').value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+          message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+          message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
